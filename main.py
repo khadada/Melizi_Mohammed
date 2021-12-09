2a043c6668e089cc6d67c555cd5287d5711e3c09
khadada <lkhadada@gmail.com>
2021-12-09 07:56:03 +0100

    I add title to the project and pack it to the window

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import *
 root = Tk()
 root.title("برنامج الأستاذ مليزي محمد")
+root.configure(padx=40,pady=40)
 root.geometry('800x800+0+0')
+# -----------------------------------------
+# ----------- Variable --------------------
+#------------------------------------------
+title_font = ("Traditional Arabiv",28,'bold')
 
 
+
+# title-------------------------------------
+title = Label(text="مرحبا مليزي محمد", font=title_font).pack()
 
 
 
