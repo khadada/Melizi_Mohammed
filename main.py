7928c1bd0b6be46e050380b8b1ba3e1db38f66d0
khadada <lkhadada@gmail.com>
2021-12-09 09:26:52 +0100

    I comment the code lines for the image background becouse I font bug in label background I don't know how to set it background to transparent

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from tkinter import *
 root = Tk()
 root.title("برنامج الأستاذ مليزي محمد")
-root.configure(padx=40,pady=40)
+root.configure(padx=0,pady=0)
 root.geometry('800x800+0+0')
+win_bg_color = '#018cb6'
+win_font_color = "#333"
+root['background'] = win_bg_color
+
+
 # -----------------------------------------
 # ----------- Variable --------------------
 #------------------------------------------
-title_font = ("Traditional Arabiv",28,'bold')
+title_font = ("Traditional Arabiv",35,'bold')
 
+# bg_img = PhotoImage(file="./particle.png")
 
+# lab_background=Label(root, image=bg_img).place(x=0,y=0)
 
 # title-------------------------------------
-title = Label(text="مرحبا مليزي محمد", font=title_font).pack()
+title = Label(text="مرحبا مليزي محمد", font=title_font,bg=win_bg_color,fg=win_font_color).pack()
 
 
 
