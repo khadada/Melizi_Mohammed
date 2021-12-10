9fb9490a9323c1ebb0f88333a227790d1813c4c4
khadada <lkhadada@gmail.com>
2021-12-10 08:26:17 +0100

    I create python file sources_vars.py to hold usefuly variables and and add to create add fontionalitiy to the app

---
+++ add_module.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from sources_vars import *
+add_win = Tk()
+add_win.geometry("800x800+0+0")
+add_win["background"] = win_bg_color
+add_win.title('مليزي محمد                                      إضافة طالب')
+#------ title -------------------------------------
+#------ subtitle ----------------------------------
+#------ container ---------------------------------
+#------ footer ------------------------------------
+
+
+add_win.mainloop()
---
+++ sources_vars.py
@@ -0,0 +1,13 @@
+# variables -------------------------------------
+win_bg_color = '#3CCBF7'
+grey_c = "#2C2626"
+white_c = '#fff'
+green_c = "#13DF8D"
+red_c = "#F35D5D"
+purple_c = "#B281FD"
+#fonts--------------------------
+title_font = ("Dubai",48,'bold')
+sub_title = ("Dubai",16,'bold')
+footer_font = ("Dubai",10,'bold')
+btn_font = ("Dubai",16,'bold')
+# -------------------------------------------------------
